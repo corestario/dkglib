e9e57fbf9c2d33a798023a27453a02418b33f856
andrey.rybnov <ryban92@gmail.com>
2019-09-09 11:17:54 +0300

    Add dkg_dealer

--- lib/dkg.go
+++ lib/dkg.go
@@ -3,23 +3,40 @@ package lib
 import (
 	"bytes"
 	"encoding/gob"
+	"errors"
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtxb "github.com/dgamingfoundation/cosmos-utils/client/authtypes"
 	"github.com/dgamingfoundation/cosmos-utils/client/context"
 	"github.com/dgamingfoundation/cosmos-utils/client/utils"
+	"github.com/dgamingfoundation/dkglib/lib/types"
 	"github.com/dgamingfoundation/randapp/x/randapp"
-	"github.com/tendermint/tendermint/consensus"
 	"github.com/tendermint/tendermint/libs/events"
 	"github.com/tendermint/tendermint/libs/log"
-	"github.com/tendermint/tendermint/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
+
+var (
+	ErrDKGVerifierNotReady = errors.New("verifier not ready yet")
+)
+
+type DKGDataMessage struct {
+	Data *types.DKGData
+}
+
+func (m *DKGDataMessage) ValidateBasic() error {
+	return nil
+}
+
+func (m *DKGDataMessage) String() string {
+	return fmt.Sprintf("[Proposal %+v]", m.Data)
+}
 
 type OnChainDKG struct {
 	cli       *context.Context
 	txBldr    *authtxb.TxBuilder
-	dealer    consensus.Dealer
+	dealer    Dealer
 	typesList []types.DKGDataType
 }
 
@@ -72,7 +89,7 @@ func (m *OnChainDKG) ProcessBlock() (error, bool) {
 		}
 	}
 
-	if _, err := m.dealer.GetVerifier(); err == consensus.ErrDKGVerifierNotReady {
+	if _, err := m.dealer.GetVerifier(); err == ErrDKGVerifierNotReady {
 		return nil, false
 	} else if err != nil {
 		return fmt.Errorf("DKG round failed: %v", err), false
@@ -81,12 +98,12 @@ func (m *OnChainDKG) ProcessBlock() (error, bool) {
 }
 
 func (m *OnChainDKG) StartRound(
-	validators *types.ValidatorSet,
-	pv types.PrivValidator,
+	validators *tmtypes.ValidatorSet,
+	pv tmtypes.PrivValidator,
 	eventFirer events.Fireable,
 	logger log.Logger,
 	startRound int) error {
-	m.dealer = consensus.NewDKGDealer(validators, pv, m.sendMsg, eventFirer, logger, startRound)
+	m.dealer = NewDKGDealer(validators, pv, m.sendMsg, eventFirer, logger, startRound)
 	if err := m.dealer.Start(); err != nil {
 		return fmt.Errorf("failed to start dealer: %v", err)
 	}
