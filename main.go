f86ddd7af875c0ebb27a87741d1f491582968362
andrey.rybnov <ryban92@gmail.com>
2019-09-09 14:08:14 +0300

    Fix to 1 val

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/user"
 	"path"
-	"strconv"
-	"sync"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/client/keys"
@@ -55,56 +54,43 @@ func MakeCodec() *codec.Codec {
 }
 
 func main() {
+	numPtr := flag.String("num", "0", "a string number")
+	flag.Parse()
+
 	var (
 		mockF  = &MockFirer{}
 		logger = log.NewTMLogger(os.Stdout)
-		vals   []*types.Validator
-		pvals  []types.PrivValidator
 	)
-	for i := 0; i < 4; i++ {
-		v, pv := getValidatorEnv()
-		vals, pvals = append(vals, v), append(pvals, pv)
-
+	val, pval := getValidatorEnv()
+	num := "0"
+	if numPtr != nil {
+		num = *numPtr
 	}
 
-	var mu sync.Mutex
-	MP := make(map[types.PrivValidator]lib.OnChainDKG)
-	wg := &sync.WaitGroup{}
-	for k, pval := range pvals {
-		cli, txBldr, err := getTools(strconv.Itoa(k))
-		if err != nil {
-			fmt.Printf("failed to get a randapp client: %v", err)
-			os.Exit(1)
-		}
+	cli, txBldr, err := getTools(num)
+	if err != nil {
+		fmt.Printf("failed to get a randapp client: %v", err)
+		os.Exit(1)
+	}
 
-		wg.Add(1)
-
-		oc := lib.NewOnChainDKG(cli, txBldr)
-		pv := pval
-		mu.Lock()
-		MP[pv] = *oc
-		mu.Unlock()
-		go func(pval types.PrivValidator) {
-			if err := oc.StartRound(types.NewValidatorSet(vals), pval, mockF, logger, 0); err != nil {
+	oc := lib.NewOnChainDKG(cli, txBldr)
+	if err := oc.StartRound(types.NewValidatorSet([]*types.Validator{val}), pval, mockF, logger, 0); err != nil {
+		panic(fmt.Sprintf("failed to start round: %v", err))
+	}
+	tk := time.NewTicker(time.Millisecond * 3000)
+	for {
+		select {
+		case <-tk.C:
+			if err, ok := oc.ProcessBlock(); err != nil {
 				panic(fmt.Sprintf("failed to start round: %v", err))
+			} else if ok {
+				fmt.Println("All instances finished DKG, O.K.")
+
+				return
 			}
-			tk := time.NewTicker(time.Millisecond * 3000)
-			for {
-				select {
-				case <-tk.C:
-					if err, ok := oc.ProcessBlock(); err != nil {
-						panic(fmt.Sprintf("failed to start round: %v", err))
-					} else if ok {
-						wg.Done()
-						return
-					}
-				}
-			}
-		}(pval)
+		}
 	}
 
-	wg.Wait()
-	fmt.Println("All instances finished DKG, O.K.")
 }
 
 func getValidatorEnv() (*types.Validator, types.PrivValidator) {
@@ -118,6 +104,7 @@ func getTools(vName string) (*context.Context, *authtxb.TxBuilder, error) {
 	if err != nil {
 		return nil, nil, err
 	}
+
 	ctx = ctx.WithCodec(cdc)
 	addr, _, err := context.GetFromFields(validatorName+vName, cliHome+vName)
 	if err != nil {
