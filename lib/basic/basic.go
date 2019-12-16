758cd19a1fe4503503aa3f0c0f31bb5698ce07c0
oopcode <oopcode@yandex.ru>
2019-12-16 21:36:53 +0300

    feat: explicit run on-chain

--- lib/basic/basic.go
+++ lib/basic/basic.go
@@ -95,7 +95,7 @@ func (m *DKGBasic) HandleOffChainShare(
 
 		// try on-chain till success
 		for {
-			if m.runOnChainDKG(validators, logger) {
+			if m.RunOnChainDKG(validators, logger) {
 				break
 			}
 		}
@@ -109,7 +109,7 @@ func (m *DKGBasic) HandleOffChainShare(
 	return false
 }
 
-func (m *DKGBasic) runOnChainDKG(validators *types.ValidatorSet, logger log.Logger) bool {
+func (m *DKGBasic) RunOnChainDKG(validators *types.ValidatorSet, logger log.Logger) bool {
 	err := m.onChain.StartRound(
 		validators,
 		m.offChain.GetPrivValidator(),
@@ -141,6 +141,10 @@ func (m *DKGBasic) CheckDKGTime(height int64, validators *types.ValidatorSet) {
 
 func (m *DKGBasic) SetVerifier(verifier dkg.Verifier) {
 	m.offChain.SetVerifier(verifier)
+}
+
+func (m *DKGBasic) SetOnChain(isOnChain bool) {
+	m.isOnChain = isOnChain
 }
 
 func (m *DKGBasic) Verifier() dkg.Verifier {
